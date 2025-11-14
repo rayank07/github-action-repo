e906c921c962ed2228ec3b31c706fd76b6185524
Rayank <102232043+rayank07@users.noreply.github.com>
2025-11-14 14:06:21 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,6 +13,7 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami           = "ami-049442a6cf8319180" # Replace with your desired AMI ID
   instance_type = "t2.micro"
+  subnet_id     = "subnet-08604f9dfa8ef0372"
 
   tags = {
     Name = "create-via-githubAction"
